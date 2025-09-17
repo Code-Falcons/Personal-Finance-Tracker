16205baf49dc8624c335e7d5a9688672aac54ff6
Bahaa Abbas <bahaaisl566@gmail.com>
2025-09-17 08:09:32 +0300

    Get Categories API

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -2,7 +2,6 @@ import categoryModel from "../models/categoryModel.js";
 
 export const createCategory = async (req, res, next) => {
   try {
-    console.log("ssada");
     const { userId, name } = req.body;
 
     const existing = await categoryModel.findByUserAndName(userId, name);
@@ -15,6 +14,30 @@ export const createCategory = async (req, res, next) => {
 
     const category = await categoryModel.create({ userId, name });
     res.status(201).json(category);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getCategories = async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const categories = await Category.find({ userId }).sort({ name: 1 });
+    res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getCategoryById = async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      const error = new Error("Category not found");
+      err.statusCode = 404;
+      return next(error);
+    }
+    res.json(category);
   } catch (error) {
     next(error);
   }
--- server/routers/categoryRouter.js
+++ server/routers/categoryRouter.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { createCategory } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  getCategories,
+  getCategoryById,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 router.post("/", createCategory);
+router.get("/:userId", getCategories);
+router.get("/single/:id", getCategoryById);
 
 export default router;
