0fb2b4f41cebd3f9edb844b620a508251b493a29
Mariam Hasanat <hasanattmariam@gmail.com>
2025-09-21 17:30:17 +0300

    delete folder backend

--- backend/middlewares/notFound.js
+++
@@ -1,7 +0,0 @@
-const notFound = (req, res, next) => {
-  const error = new Error("Not Found - 404");
-  error.status = 404;
-  next(error);
-};
-
-export default notFound;
--- backend/routers/userRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { printUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.get("/", printUser);
-
-export default router;
